ed48c0f404d3c9102a7e41f2cc656e98d9d8a401
Pavle Lazarevic <pavleslazarevi@gmail.com>
2023-02-27 01:06:02 +0100

    Konacno, napravio da kasa radi

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ function App() {
 
   const [stoIznos, setStoIznos] = useState(0);
   const [stoId, setStoId] = useState(1);
-  //const [stolovi, setStolovi] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [stolovi, setStolovi] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  //const stolovi = [0, 0, 0, 0, 0, 0, 0, 0];
 
   function onNazad(iznos, brStola) {
     // console.log(iznos);
     // console.log(brStola);
-    console.log("nazad");
-    console.log(stolovi[brStola - 1]);
+    const pom = stolovi;
+    pom[brStola - 1] = iznos;
+    setStolovi(pom);
+    //console.log(pom);
+    //console.log("nazad");
+    //console.log(stolovi[brStola - 1]);
   }
 
   function dajBroj(broj) {
@@ -23,7 +28,7 @@ function App() {
     // console.log(stoIznos);
     // console.log(broj);
     console.log(stolovi);
-    setStoIznos(stoIznos + stolovi[broj - 1]);
+    setStoIznos(stolovi[broj - 1]);
     setStoId(broj);
   };
 
